617dbab79f3feb8e5efa13e8c0af669a208239e1
FreddyFish <Pepsai@live.dk>
2017-04-18 12:30:28 +0200

    Works

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var quotes=new Array();
 quotes[0] = "Kheo's kød siger: Ja";
 quotes[1] = "Kheo's kød siger: Nej";
+quotes[2] = "Keho's kød siger: Ved ikke";
 
 var q = quotes.length;
 var whichquote=Math.round(Math.random()*(q-1));
